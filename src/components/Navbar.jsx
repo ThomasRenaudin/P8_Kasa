95412562fbeb5562297784492519dd77f5da075c
Thomas Renaudin <thomas.renaudin79000@gmail.com>
2023-11-07 16:02:44 +0100

    corrections soutenance

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import "../css/Navbar.css";
-
+import { NavLink } from 'react-router-dom';
 
 
 function Navbar() {
@@ -10,11 +10,13 @@ function Navbar() {
             <img className="navbar__logo--image" alt="logo" src='/LOGO.png'></img>
         </div>
         <div className="navbar__liens">
-            <a href="/" className="navbar__liens--accueil">Accueil</a>
-            <a href="/about" className="navbar__liens--apropos">A propos</a>
+        <NavLink to="/" className="navbar__liens--accueil">Accueil</NavLink>
+        <NavLink to="/about" className='navbar__liens--apropos'>A propos</NavLink>
         </div>
     </nav>
   )
 }
 
+
 export default Navbar
+
--- src/components/logements.jsx
+++ src/components/logements.jsx
@@ -1,33 +1,40 @@
 import React from 'react';
 import "../css/logements.css";
 import logementsData from '../backend.json';
+import { NavLink } from 'react-router-dom';
 
 function Logements() {
-  
   let logementsToShow;
 
-
-  if (window.innerWidth <= 768){
+  if (window.innerWidth <= 768) {
     logementsToShow = logementsData.slice(0, 3);
   } else {
     logementsToShow = logementsData.slice(0, 6);
   }
-  
 
   return (
     <div className="Logements">
-      {logementsToShow.map(logement => (
-        <a href={`/appartments/${logement.id}`} className="Logements__container" key={logement.id}>
-            <img className="Logements__container--image" src={logement.pictures[0]} alt={logement.title} />
-            <div className="Logements__container--flou"></div>
-            <h2 className="Logements__container--titre">{logement.title}</h2>
-        </a>
+      {logementsToShow.map((logement) => (
+        <NavLink
+          to={`/appartments/${logement.id}`}
+          className="Logements__container"
+          key={logement.id}
+        >
+          <img
+            className="Logements__container--image"
+            src={logement.pictures[0]}
+            alt={logement.title}
+          />
+          <div className="Logements__container--flou"></div>
+          <h2 className="Logements__container--titre">{logement.title}</h2>
+        </NavLink>
       ))}
     </div>
   );
 }
 
 export default Logements;
+
 
   
   
